6fe58ff626fc1c962bfc48c1d39b803dd3406db3
2TB-png <nikola.cat1000@gmail.com>
2025-01-22 14:02:31 +0100

    In proggres

--- basic_UI.py
+++ basic_UI.py
@@ -1,14 +1,34 @@
 import pygame
 
-class Hitbox:
+class ButtonHitbox:
     def __init__(self, width, height, x, y):
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-    def update(self):
-        pass
+        self.debug_mode = False
+        self.screen = None
+
+        self.is_down = False
+        self.was_down = False
+        self.is_active = False
+
+    def update(self, mouse):
+
+        mouse = pygame.mouse
+
+
+    def __handle_activation__(self, mouse):
+
+        mouse_pos = mouse.get_pos()
+        mouse_pressed = mouse.get_pressed()
+        self.is_active = False
+
+        if
+
+        if not self.is_down and self.was_down:
+            self.is_active =True
 
     def change_pos(self, x, y):
         pass
@@ -34,5 +54,6 @@ class Hitbox:
     def on_activation(self, func, args=()):
         pass
 
-    def set_debug(self, screen):
-        pass
+    def set_debug(self, screen, activate=True):
+        self.debug_mode = activate
+        self.screen = screen
